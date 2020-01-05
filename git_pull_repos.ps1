7bcd7ef6f7b0d457cc1046b78d077978c94ff8c6
Jan Kostrzewa <jankostrzewa@users.noreply.github.com>
2020-01-05 22:24:28 +0100

    Modified sh, added ps1

--- git_pull_repos.ps1
+++ git_pull_repos.ps1
@@ -3,4 +3,4 @@ git clone https://github.com/jankostrzewa/System.git #master repo with scripts
 git clone https://github.com/jankostrzewa/Service.git #backend service
 git clone https://github.com/jankostrzewa/frontend_react.git
 git clone https://github.com/jankostrzewa/frontend_angular.git
-git clone https://github.com/jankostrzewa/frontend_javascript.git
+git clone https://github.com/jankostrzewa/frontend_javascript.gitd
