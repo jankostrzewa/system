dfd4448bfb2afdee966a7f99392c8c3332e37f92
Jan Kostrzewa <jankostrzewa@users.noreply.github.com>
2020-01-05 21:18:10 +0100

    initial commit

---
+++ git_pull_repos.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/jankostrzewa/System.git #master repo with scripts
+git clone https://github.com/jankostrzewa/Service.git #backend service
+git clone https://github.com/jankostrzewa/frontend_react.git
+git clone https://github.com/jankostrzewa/frontend_angular.git
+git clone https://github.com/jankostrzewa/frontend_javascript.git
